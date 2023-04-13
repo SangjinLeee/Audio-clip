94febea45c7558653b511a7a5d114ea64c3ada48
sfl5419 <122560041+SangjinLeee@users.noreply.github.com>
2023-04-13 12:28:22 -0400

    first setting

---
+++ audio-clip/stories/audio-clip.stories.js
@@ -0,0 +1,25 @@
+import { html } from 'lit';
+import '../src/audio-clip.js';
+
+export default {
+  title: 'AudioClip',
+  component: 'audio-clip',
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+};
+
+function Template({ title, backgroundColor }) {
+  return html`
+    <audio-clip
+      style="--audio-clip-background-color: ${backgroundColor || 'white'}"
+      .title=${title}
+    >
+    </audio-clip>
+  `;
+}
+
+export const App = Template.bind({});
+App.args = {
+  title: 'My app',
+};
